17fa2dfd5d136814da71fe3698dbc8e87fde2b55
Aleksandr Berezhnoy <alex@MacBook-Pro-Aleksandr.local>
2023-09-10 14:51:52 +0300

    cs project added

---
+++ cs/MyLib.cs
@@ -0,0 +1,25 @@
+namespace MyLib
+{
+    class MyLibClass
+    {
+        public static string Propmt(string message)
+        {
+            Console.WriteLine(message);
+            return Console.ReadLine();
+        }
+        public static int Propmt()
+        {
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        public static void PrintArray(string[] strings)
+        {
+            Console.Write("[ ");
+            for (int i = 0; i < strings.Length; i++)
+            {
+                Console.Write($"{strings[i]} ");
+            }
+            Console.Write("]");
+        }
+    }
+}
---
+++ cs/Program.cs
@@ -0,0 +1,62 @@
+﻿using MyLib;
+
+namespace FinalTestOnTheMainBlock;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter array size: ");
+        string[] sourceArray = new string[MyLibClass.Propmt()];
+
+        for (int i = 0; i < sourceArray.Length; i++)
+        {
+            sourceArray[i] = MyLibClass.Propmt($"Enter {i + 1} string: ");
+        }
+
+        string[] destinationArray = GetDstArrayInOneCycle(sourceArray);
+
+        Console.Write($"In source array: [ {string.Join(", ", sourceArray)} ] strings less than four characters long: [ {string.Join(", ", destinationArray)} ]");
+    }
+
+    public static string[] GetDstArray(string[] srcArray)
+    {
+        int size = 0;
+        for (int i = 0; i < srcArray.Length; i++)
+        {
+            if (srcArray[i].Length <= 3)
+            {
+                size++;
+            }
+        }
+        string[] dstArray = new string[size];
+        for (int i = 0, j = 0; i < srcArray.Length; i++)
+        {
+            if (srcArray[i].Length <= 3)
+            {
+                dstArray[j] = srcArray[i];
+                j++;
+            }
+        }
+
+        return dstArray;
+    }
+    public static string[] GetDstArrayInOneCycle(string[] srcArray)
+    {
+        int size = 0;
+        string[] dstArray = new string[size];
+        for (int i = 0, j = 0; i < srcArray.Length * 2 + 1; i++)
+        {
+            if (i < srcArray.Length && srcArray[i].Length <= 3)
+            {
+                size++;
+            }
+            else if (i == srcArray.Length) dstArray = new string[size];
+            else if (i > srcArray.Length && srcArray[i - srcArray.Length - 1].Length <= 3)
+            {
+                dstArray[j++] = srcArray[i - srcArray.Length - 1];
+            }
+        }
+        return dstArray;
+    }
+}
